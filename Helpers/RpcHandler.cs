65fe27b810cd49006b6205d31893b44f6eb9eb45
Barani Kumar S <baranikumars2003@gmail.com>
2023-01-07 20:32:08 +0530

    Created RpcHandler.cs

--- Helpers/RpcHandler.cs
+++ Helpers/RpcHandler.cs
@@ -1,6 +1,32 @@
-﻿namespace UncannyRPC.Helpers;
+﻿using System;
+using DiscordRPC;
+using DiscordRPC.Logging;
+
+namespace UncannyRPC.Helpers;
 
 public class RpcHandler
 {
-    
+    private readonly long token;
+    private readonly DiscordRpcClient Client;
+
+    public RpcHandler(long token)
+    {
+        Client = new(this.token.ToString());
+        Initializer();
+    }
+
+    private void Initializer()
+    {
+        Client.Logger = new ConsoleLogger()
+        {
+            Level = LogLevel.Warning | LogLevel.Error | LogLevel.Info
+        };
+
+        Client.OnReady += (sender, args) =>
+        {
+            Console.WriteLine($"{args.User.Username} is ready...");
+        };
+
+        Client.Initialize();
+    }
 }
