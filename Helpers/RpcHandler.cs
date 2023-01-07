e5506ae465b7ec3fbace2653ee92bd331793b72d
Barani Kumar S <baranikumars2003@gmail.com>
2023-01-07 21:00:11 +0530

    Added PresenceObject

--- Helpers/RpcHandler.cs
+++ Helpers/RpcHandler.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Timers;
 using DiscordRPC;
 using DiscordRPC.Logging;
 
@@ -8,6 +10,7 @@ public class RpcHandler
 {
     private readonly long token;
     private readonly DiscordRpcClient Client;
+    private readonly Timer timer = new (1000);
 
     public RpcHandler(long token)
     {
@@ -29,4 +32,46 @@ public class RpcHandler
 
         Client.Initialize();
     }
+
+    private void SetPresence(
+        )
+    {
+        Client.SetPresence(new RichPresence()
+        {
+            Details = string.Format(
+                "{0} {1} {2}",
+                cpuTitle, cpuSeperator, cpuCurrent),
+            State = string.Format(
+                "{0} {1} {2}/{3}GB",
+                ramTitle,
+                ramSeperator,
+                ramCurrent,
+                ramTotal),
+            Assets = new Assets()
+            {
+                LargeImageKey = imageSource,
+                LargeImageText = "UncannyRPC"
+            }
+        });
+    }
+
+    public async Task RunPresence()
+    {
+        timer.Elapsed += (sender, args) =>
+        {
+            
+        };
+    }
+}
+
+public class PresenceObject
+{
+    public string cpuTitle { get; set; }
+    public string ramTitle { get; set; }
+    public string imageSource { get; set; }
+    public string cpuSeperator { get; set; }
+    public string ramSeperator { get; set; }
+    public string cpuCurrent { get; set; }
+    public string ramCurrent { get; set; }
+    public string ramTotal { get; set; }
 }
